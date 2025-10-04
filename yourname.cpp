c8050654fc17006a927a4a37a8106bd1d4bb3e4a
zarsich <zarsich@gmail.com>
2025-10-04 12:37:01 +0300

    Imba

---
+++ yourname.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Arsewa on 4.10.25.
+//
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Скики тебе годиков?"<<endl;
+    cin>>n;
+    cout<<"Значит тебе вот стока годиков: "<<n<<endl;
+}
